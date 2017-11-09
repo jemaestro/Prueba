fb68974caea1206c277f8ada46f042922c198079
Jesús <jesusinfdam@gmail.com>
2017-11-09 13:23:07 +0100

    Commit inicial

---
+++ problema_tabla7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	int veces=0;
+	
+	while (veces <= 10) {
+		cout << "7 x " << veces << " = " << 7*veces << endl;
+		veces = veces ++;
+	} 
+}	
